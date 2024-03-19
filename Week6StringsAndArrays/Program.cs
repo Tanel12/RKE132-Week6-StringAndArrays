0a9667603e4e7bfaad3dfd7d80e06ccdcc29c662
Tanel Treimann <tanel.treimann@tktk.ee>
2024-03-19 21:21:03 +0200

    add week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+string hello = "  gello, world? lel";
+
+int stringLenght = hello.Length;
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+
+
+
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);   
+
+
+
+//int wordCounter = 1;
+
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+    
+//}
+
+//Console.WriteLine($"word count in {hello}: {wordCounter} words.");
